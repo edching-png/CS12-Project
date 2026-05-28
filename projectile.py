cc6f9854f5994491a4f3b9a877851c1d8c159ad2
edching-png <edching@up.edu.ph>
2026-05-28 16:56:28 +0800

    Create projectile.py

---
+++ projectile.py
@@ -0,0 +1,11 @@
+class Projectile:
+    def __init__(self, x, y, angle, speed=2.4):
+        self.x = x
+        self.y = y
+        self.vx = math.cos(angle) * speed
+        self.vy = math.sin(angle) * speed
+        self.radius = 5
+
+    def update(self):
+        self.x += self.vx
+        self.y += self.vy
